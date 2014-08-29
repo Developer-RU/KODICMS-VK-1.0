f9d7a6b76bfead40dbc7adaba3c3612a34187532
Павел <p.masyukov@yandex.ru>
2014-08-29 07:43:37 +0600

    Commit

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
+
+Plugin::factory('vkgroup', array(
+	'title' => 'Vkgroup',
+	'description' => 'Виджет группы вконтакте',
+	'version' => '1.0.0',
+))->register();
